f61bc313b94fa0a301263f8f54a77ad32a697834
michaeljohn007 <69838837+michaeljohn007@users.noreply.github.com>
2020-09-25 07:58:23 +0100

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -56,5 +56,9 @@ def main():
 
     ax.bar(labels, values, color=['green','red','blue'])
 
+    ax.set_title('Your total expenses vs. total budget')
+
+    plt.show()
+
 if __name__ == "__main__":
     main()
