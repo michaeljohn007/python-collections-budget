82dfdff09d5599d0f971e41680e5acea48828611
michaeljohn007 <69838837+michaeljohn007@users.noreply.github.com>
2020-09-25 06:48:15 +0100

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -20,6 +20,13 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __iter__(self):
+        iter(self.expenses)
+        self.iter_0 = iter(self.overages)
+        return self
+
+    
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
