a713a9b7cb1c327a5657b94d77254fd3a52bd25c
michaeljohn007 <69838837+michaeljohn007@users.noreply.github.com>
2020-09-25 07:56:42 +0100

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -54,5 +54,7 @@ def main():
 
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
 
+    ax.bar(labels, values, color=['green','red','blue'])
+
 if __name__ == "__main__":
     main()
